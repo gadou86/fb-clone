328bd33cc024bfc690b931eb227d598c13497bee
Gad Dayan <70294896+gadou86@users.noreply.github.com>
2021-04-30 13:53:51 -0400

    fb-clone

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Header.css"
+
+function Header() {
+  return (
+    <div className="Header">
+      <div className="header__left"></div>
+
+      <div className="header__midle"></div>
+
+      <div className="header__right"></div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBy47kQtNwA61H2gTZNpC70_MoUrUrMnaQ",
+  authDomain: "facebook-clone-8009f.firebaseapp.com",
+  projectId: "facebook-clone-8009f",
+  storageBucket: "facebook-clone-8009f.appspot.com",
+  messagingSenderId: "80807722700",
+  appId: "1:80807722700:web:ff14252a228cf445964c69"
+};
