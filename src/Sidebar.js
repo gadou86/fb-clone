70afb39e0dd472716d8834d3938b559ec796a26a
Gad Dayan <70294896+gadou86@users.noreply.github.com>
2021-04-30 20:08:18 -0400

    added story and storyReel

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -12,7 +12,7 @@ import ExpandMoreOutlinedIcon from '@material-ui/icons/ExpandMoreOutlined';
 function Sidebar() {
   return (
     <div className="sidebar">
-      <SidebarRow src="" title="Gad Dayan" />
+      <SidebarRow src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80" title="Gad Dayan" />
       <SidebarRow Icon={LocalHospitalIcon} title='COVID-19 Information Center'/>
       <SidebarRow Icon={EmojiFlagsIcon} title='Pages'/>
       <SidebarRow Icon={PeopleIcon} title='Friends'/>
---
+++ src/StoryReel.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import './StoryReel.css'
+import Story from './Story'
+
+function StoryReel() {
+  return (
+    <div className="storyReel">
+      <Story 
+        image="https://images.unsplash.com/photo-1571388878919-2d2214775af4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80"
+        profileSrc="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80"
+        title="Gad"
+      />
+
+      <Story 
+        image="https://images.unsplash.com/photo-1530878955558-a6c31b9c97db?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80"
+        profileSrc="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80"
+        title="Laurent Maman"
+      />
+
+      <Story 
+        image="https://images.unsplash.com/photo-1503696967350-ad1874122058?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80"
+        profileSrc="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80"
+        title="Victoria Havill"
+      />
+
+      <Story 
+        image="https://images.unsplash.com/photo-1617947775519-c0f15d1358db?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=752&q=80"
+        profileSrc="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80"
+        title="Patrick Cohen"
+      />
+            <Story 
+        image="https://images.unsplash.com/photo-1602950045706-29cef8e7bff9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2507&q=80"
+        profileSrc="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80"
+        title="David Sasportas"
+      />
+    </div>
+  )
+}
+
+export default StoryReel
