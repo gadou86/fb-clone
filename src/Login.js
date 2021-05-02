58a2ff6e8f10e72759e7ca330d29ef6c70f14d2f
Gad Dayan <70294896+gadou86@users.noreply.github.com>
2021-05-01 22:00:53 -0400

    added login with firebase

---
+++ src/Login.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import './Login.css'
+import {Button} from '@material-ui/core'
+import { auth, provider } from './firebase';
+import { actionTypes } from './reducer';
+import { useStateValue } from './StateProvider';
+
+function Login() {
+  const [{user}, dispatch] = useStateValue();
+
+  const signIn = () => {
+    auth.signInWithPopup(provider)
+    .then((result) => {
+      dispatch({
+        type: actionTypes.SET_USER,
+        user: result.user,
+      });
+      console.log(result);
+    })
+    .catch((error) => alert(error.message))
+  };
+
+
+  return (
+    <div className="login">
+        <div className="login__logo">
+          <img src="https://facebookbrand.com/wp-content/uploads/2019/04/f_logo_RGB-Hex-Blue_512.png?w=512&h=512" alt=""/>
+          <img src="https://www.logo.wine/a/logo/Facebook/Facebook-Logo.wine.svg" alt=""/>
+        </div>
+        <Button type="submit" onClick={signIn}
+        >
+        Sign in
+        </Button>
+    </div>
+  )
+}
+
+export default Login
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase'
+
 const firebaseConfig = {
   apiKey: "AIzaSyBy47kQtNwA61H2gTZNpC70_MoUrUrMnaQ",
   authDomain: "facebook-clone-8009f.firebaseapp.com",
@@ -6,3 +8,11 @@ const firebaseConfig = {
   messagingSenderId: "80807722700",
   appId: "1:80807722700:web:ff14252a228cf445964c69"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
